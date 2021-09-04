1ad8d40d435602a7ed438fd276c913763ccbcdb5
RamanRabinovich <forromantest@gmail.com>
2021-09-04 13:16:13 +0300

    second commit

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import { AddTodo } from './src/AddTodo';
 import { Navbar } from './src/Navbar';
 import { Todo } from './src/Todo';
@@ -8,35 +8,34 @@ import { Todo } from './src/Todo';
 export default function App() {
   const [todos, setTodos] = useState([])
 
-  const addTodo = (title) =>{
-    // const newTodo ={
-    // id: Date.now().toString(),
-    // title: title
-    // }
-
-    // setTodos((prevTodos) => {
-    //   return [
-    //     ...prevTodos,
-    //     newTodo
-    //   ]
-    // })
-
-    setTodos(prev => [...prev, {
+  const addTodo = title => {
+    setTodos(prev => [
+      ...prev, 
+      {
       id: Date.now().toString(),
       title
-    }])
+      }
+   ])
+  }
+
+  const removeTodo = id => {
+    setTodos(prev => prev.filter(todo => todo.id !== id))
   }
 
   return (
     <View>
       <Navbar title='Todo App, Beach !!'/>
-      <View style={styles.container}>
-      <AddTodo onSubmit={addTodo}/>
-      
-        <View>
-          {todos.map(todo => (<Todo todo={todo} key={todo.id} />) )}
+        <View style={styles.container}>
+          <AddTodo onSubmit={addTodo}/>
+
+          <FlatList
+            keyExtractor={item => item.id.toString()}
+            data={todos}
+            renderItem={({ item }) => <Todo todo={item} onRemove={removeTodo} />
+          }
+          />
+                
         </View>
-      </View>
     </View>
   )
 }
